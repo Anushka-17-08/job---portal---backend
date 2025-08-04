071d9f19105cd527b843279a7c7a7efcca23a40f
Anushka-17-08 <anushkamazumdar11@gmail.com>
2025-08-05 01:32:45 +0530

    Update server config and add .env handling

--- server.js
+++ server.js
@@ -48,7 +48,8 @@ const pageRoutes = require('./routes/pageRoutes');
 app.use('/', pageRoutes);
 //console.log(require('crypto').randomBytes(64).toString('hex'))
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
